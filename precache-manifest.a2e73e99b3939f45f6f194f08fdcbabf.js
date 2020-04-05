22af334ed1bb2b712a6c0b07e3750d99450cc9f7
sdf <dfvsdfvadv>
2020-04-05 12:19:23 +0200

    Updates

---
+++ precache-manifest.a2e73e99b3939f45f6f194f08fdcbabf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336baa726ca5171ae72037c2253fa17e",
+    "url": "/ToDo-Publish-App/index.html"
+  },
+  {
+    "revision": "717ab6dfac7fa54a1c24",
+    "url": "/ToDo-Publish-App/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c027ebfb52d684233df8",
+    "url": "/ToDo-Publish-App/static/css/main.013c40eb.chunk.css"
+  },
+  {
+    "revision": "717ab6dfac7fa54a1c24",
+    "url": "/ToDo-Publish-App/static/js/2.78adff81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-Publish-App/static/js/2.78adff81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c027ebfb52d684233df8",
+    "url": "/ToDo-Publish-App/static/js/main.3bd8c025.chunk.js"
+  },
+  {
+    "revision": "950faf5306e4f1fa6194",
+    "url": "/ToDo-Publish-App/static/js/runtime-main.5fed835f.js"
+  }
+]);
